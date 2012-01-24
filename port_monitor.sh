9adc8c6006c62908a37d87c51fc4c6e70c70df25
Diego Sapriza <diego@sapriza.com>
2012-01-24 11:29:10 -0200

    receive ports by arguments

--- port_monitor.sh
+++ port_monitor.sh
@@ -4,14 +4,15 @@ E_BADARGS=65
 
 if [ ! -n "$1" ]
 then
-  echo "Usage: port_monitor.sh domain.com"
+  echo "Usage: `basename $0` domain.com"
   exit $E_BADARGS
 fi 
 
 
 host=$1
-#host=google.com
-for port in 80 25 
+shift
+for port in "$@" 
+#for port in 80 25 
 do
   if netcat -z $host $port
   then
