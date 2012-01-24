734740736a576fd3ee9a7ece2a7b475a6b14219c
Diego Sapriza <diego@sapriza.com>
2012-01-24 11:23:52 -0200

    domain in argument

--- port_monitor.sh
+++ port_monitor.sh
@@ -1,5 +1,17 @@
-host=google.com
-for port in  80 25 
+#!/bin/bash
+
+E_BADARGS=65
+
+if [ ! -n "$1" ]
+then
+  echo "Usage: port_monitor.sh domain.com"
+  exit $E_BADARGS
+fi 
+
+
+host=$1
+#host=google.com
+for port in 80 25 
 do
   if netcat -z $host $port
   then
